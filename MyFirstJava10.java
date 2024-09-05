2d7f4bd276f9da4969d0e1f02290396946a9acce
Muhammad-Zinky <mohammadzinky@gmail.com>
2024-09-05 21:32:59 +0700

    Add files via upload

---
+++ MyFirstJava10.java
@@ -0,0 +1,15 @@
+public class MyFirstJava10 {
+    public static void main(String[] args){
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama      : muhammad atabik zinky");
+        System.out.print("NIM       : 244107060131");
+        //System.out.print("\nsaya adalah Mahasiswa Jurusan Teknologi Informasi - POLINEMA"); 
+        System.out.println("No Absen  : 10");
+        System.out.println("Prodi     : D-IV SISTEM INORMASI BISNIS");
+        System.out.println("Alamat    : Jln.Sisingamangaraja Kepatihan Jombang");
+        System.out.println("No HP     : 082231486734");
+        System.out.println("Hobi      : Badminton");
+    }
+}
